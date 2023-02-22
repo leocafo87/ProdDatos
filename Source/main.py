6d1fc5d09412ca7b92a0fe499e960102ce131245
leocafo87 <leonardo.castellanos.forero@gmail.com>
2023-02-22 10:19:57 -0500

    Identificar los elementos de l formula y crear un df con 8 espacios

--- Source/main.py
+++ Source/main.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     dfGFA = read_files.loadGFA()
     dfElements = read_files.loadTablaPeriodica()
     find_elements.findIndexPosition(dfGFA,dfElements)
-    #formula_NotNumber.formulaWithoutNumber(dfGFA)
 
 #    if len(sys.argv) == 1:
 #        simulator.restart()
